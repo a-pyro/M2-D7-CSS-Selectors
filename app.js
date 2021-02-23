4ff1ced8cbc2f4c356231be0c6c8241684217c71
ardigermenji <ardi.germenji@gmail.com>
2021-02-23 16:59:14 +0100

    adds ex 18

--- app.js
+++ app.js
@@ -53,8 +53,39 @@ const removeLinks = (element) =>
 
 removeLinks(elseWhere);
 // EX15) Write a function to change the column size for post headings
+const blogPosts = document.getElementsByClassName('blog-post');
+[...blogPosts].forEach((post) => post.classList.add('d-flex', 'flex-column'));
+//! rivedere
+
 // EX16) Write a function to remove the "Search" magnifying glass icon
+const magnifying = document.querySelector(
+  'body > div > header > div > div.col-4.d-flex.justify-content-end.align-items-center > a.text-muted > svg'
+);
+magnifying.remove(); //!rivedere
 // EX17) Write a function to trim just the first 50 characters in the first paragraph for each blog post
+//! DA FARE
+const trim50Chars = () => {};
 // EX18) Write a function and attach it to the "Newer" button, to add new Blog Post (just div and title)
+const blogSection = document.querySelector('body > main > div > .blog-main');
+console.log(blogSection);
+const newerBtn = document.querySelector(
+  'body > main > div > div > nav > a.btn.btn-outline-secondary'
+);
+console.log(newerBtn);
+newerBtn.classList.remove('disabled');
+const createNewBlogPost = (e) => {
+  e.preventDefault();
+  const blogDiv = document.createElement('div');
+  blogDiv.classList.add('blog-post', 'd-flex', 'flex-column');
+  const heading = document.createElement('h2');
+  heading.classList.add('blog-post-title');
+  heading.innerText = `New posto title`;
+  blogDiv.appendChild(heading);
+  blogSection.insertBefore(
+    blogDiv,
+    blogSection.querySelector('.blog-pagination')
+  );
+};
+newerBtn.addEventListener('click', createNewBlogPost);
 // EX19) Write a function and attach it to the "Older" button, to remove the last Blog Post
 // EX20) Write an alert with the name of the author every time the user hover with the mouse over an author name
